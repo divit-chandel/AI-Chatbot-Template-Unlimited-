4d1633bb5cf501d956fe1b44509d1c7236817326
Divit Chandel <c6643399@gmail.com>
2025-08-09 07:47:41 +0530

    Delete list.py

--- list.py
+++
@@ -1,6 +0,0 @@
-models = [  # List of models in priority order
-    "qwen/qwen2.5-vl-72b-instruct:free",
-    "tngtech/deepseek-r1t2-chimera:free",
-    "microsoft/mai-ds-r1:free",
-    "thudm/glm-4-32b:free"
-]
